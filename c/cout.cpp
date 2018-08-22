5f7ad6e955006281fd22c380bdd14c28424203e3
Shilpa Kancharla <shilpakancharla18@gmail.com>
2018-08-21 22:55:43 -0400

    cout

---
+++ c/cout.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	char str[] = "Hello C++";
+
+	cout << "Value of str is : " << str << endl;
+}
+
+//cout is an instance of stream class. said to be "connected to" the standard
+//output device, which usually is the display screen
