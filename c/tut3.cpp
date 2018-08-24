45744318a68f841e65a05027f3e9d3e2f1cdec51
Shilpa Kancharla <shilpakancharla18@gmail.com>
2018-08-24 10:05:34 -0400

    more review

--- c/tut3.cpp
+++ c/tut3.cpp
@@ -14,5 +14,39 @@ int main() {
 
 	//arrays
 
+	int myFavNums[5]; 
+
+	int badNums[5] = {4, 13, 14, 24, 34}; //indexed at 0
+	
+	cout << "Bad Number 1: " << badNums[0] << endl;
+
+	//multidimensional array
+
+	char myName[6][9] = {{'S', 'h', 'i', 'l', 'p', 'a'}, 
+						{'K', 'a', 'n', 'c', 'h', 'a', 'r', 'l', 'a'}};
+
+	cout << "2nd letter in 2nd array: " << myName[1][1] << endl;
+
+	//first bracket for array, second bracket for location
+
+	myName[0][2] = 'e';
+
+	cout << "New Value: " << myName[0][2] << endl;
+
+	//for loop
+
+	for(int i = 1; i <= 10; i++) {
+		
+		cout << i << endl;
+	}
+
+	for(int j = 0; j < 6; j++) {
+		
+		for(int k = 0; k < 9; k++) {
+			
+			cout << myName[j][k];
+		}
+	}
+
 	return 0;
 }
