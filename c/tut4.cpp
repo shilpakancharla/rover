869ffa1e36971e836dde8c13be02a3a7b2250d9d
Shilpa Kancharla <shilpakancharla18@gmail.com>
2018-08-24 14:29:14 -0400

    strings

--- c/tut4.cpp
+++ c/tut4.cpp
@@ -39,6 +39,37 @@ int main() {
 
 	}
 
+	//getting size or number of characters in string
+	cout << "Size of String " << eulerGuess.size() << endl;
+	cout << "Is string empty? " << eulerGuess.empty() << endl; //0 if false, 1 if true 
+	cout << eulerGuess.append(" was your guess") << endl;
+
+	string dogString = "dog";
+	string catString = "cat";
+	
+	cout << dogString.compare(catString) << endl;
+	cout << dogString.compare(dogString) << endl;
+	cout << catString.compare(dogString) << endl; //receives -1
+	cout << catString.compare(catString) << endl;
+
+	string wholeName = yourName.assign(yourName);
+	cout << wholeName << endl;
+
+	string firstName = wholeName.assign(wholeName, 0, 5);
+	cout << firstName << endl;
+
+	int lastNameIndex = yourName.find("Kancharla", 0);
+	cout << "Index for last name " << lastNameIndex << endl;
+
+	yourName.insert(5, " Justin");
+	cout << yourName << endl;
+
+	yourName.erase(6, 7);
+	cout << yourName << endl;
+
+	yourName.replace(6, 5, "Maximus");
+	cout << yourName << endl;
+	
 	return 0;
 
 }
