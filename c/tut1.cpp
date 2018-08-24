1cedcfb4030d16195b0de2a2f2f545a51bc731bd
Shilpa Kancharla <shilpakancharla18@gmail.com>
2018-08-24 09:50:42 -0400

    driving

---
+++ c/tut1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+
+	//comparison operators: ==, !=, >, <, >=, =<
+	//logic operators: &&, ||, !
+
+	int age = 70;
+	int ageAtLastExam = 16;
+	bool isNotIntoxicated = true;
+
+	if((age >= 1) && (age < 16)) {
+		cout << "You can't drive" << endl;
+	} else if(! isNotIntoxicated) {
+		cout << "You can't drive" << endl;
+	} else if(age >= 80 && ((age > 100) || ((age - ageAtLastExam) > 5))) {
+		cout << "You can't drive" << endl;
+	} else {
+		cout << "You can drive" << endl;
+	}
+
+	return 0;
+}
