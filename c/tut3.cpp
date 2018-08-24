d403fd4100496c0a4afcfd590239a1ec9f88b09b
Shilpa Kancharla <shilpakancharla18@gmail.com>
2018-08-24 09:57:18 -0400

    ternary operators

---
+++ c/tut3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+
+	//ternary operator
+	//variable = (condition) ? true : false
+
+	int largestNum = (5 > 2) ? 5 : 2;
+
+	//arrays
+
+	return 0;
+}
