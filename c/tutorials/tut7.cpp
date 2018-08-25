11cebff0085964a2734044dddd1538bc04832d5a
Shilpa Kancharla <shilpakancharla18@gmail.com>
2018-08-25 09:08:11 -0400

    reader and writer

--- c/tutorials/tut7.cpp
+++ c/tutorials/tut7.cpp
@@ -7,11 +7,11 @@ using namespace std;
 
 int main() {
 
-	string steveQutoe = "A day without sunshine is like, you know, night";
+	string steveQuote = "A day without sunshine is like, you know, night";
 
 	ofstream writer("stevequote.txt");
 
-	if(! write) {
+	if(! writer) {
 		cout << "Error opening file" << endl;
 		return -1;
 	} else {
@@ -19,13 +19,37 @@ int main() {
 		writer.close();
 	}
 
-	ofstream writer2("steverquote.txt", ios::app);
+	ofstream writer2("stevequote.txt", ios::app); //appending file
 
 	// Open a stream to append to whats there with ios::app
 	// ios::binary : Treat the file as binary
 	// ios::in : Open a file to read input
 	// ios::trunc : Default
 	// ios::out : Open a file to write output
+
+	if(! writer2) {
+		cout << "Error opening file" << endl;
+		return -1;
+	} else {
+		writer2 << "\n - Steve Martin" << endl;
+		writer2.close();
+	}
+
+	char letter;
+	ifstream reader("stevequote.txt");
+	if(! reader) {
+		cout << "Error opening file" << endl;
+		return -1;
+	} else {
+		for(int i = 0; ! reader.eof(); i++) {
+			reader.get(letter);
+			cout << letter;
+		}
+		
+		cout << endl;
+		reader.close();
+
+	}
 
 	return 0;
 
